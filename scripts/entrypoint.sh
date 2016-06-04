45339c1556634ef8a76fef72c9dd8a61471a0b7c
Bruce Doan <rgv151@gmail.com>
2016-06-04 12:18:00 +0700

    update hostname for new container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -14,6 +14,9 @@ run() {
     if [ ! -f "$LOCK_FILE" ]; then
 	/scripts/install.sh $MYSQL_HOST $MYSQL_USER $MYSQL_PASSWORD $MYSQL_SCHEMA
 	touch $LOCK_FILE
+    else
+	HOSTNAME=$(hostname)
+	mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e  "UPDATE Hosts SET hostname = '$HOSTNAME' WHERE hostnum = 1;" $MYSQL_SCHEMA
     fi
     magmad $BASE_DIR/etc/magma.config
 }
